48fd39c10b1c1b55ca015f5b13d804f615dd76eb
Melanie Moreno <temgotocoleg413@gmail.com>
2024-11-15 22:27:29 -0600

    Implemented event loop

--- src/project.py
+++ src/project.py
@@ -7,6 +7,22 @@ def main():
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
 
+    running = True
+    while running:
+        # Event Loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Game Logic
+
+
+        # Render & Display
+
+
+    pygame.quit()
+
+
 
 if __name__ == "__main__":
     main()
