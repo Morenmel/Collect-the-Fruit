50143708d4b173f7a1c681056442c77597a98073
Melanie Moreno <temgotocoleg413@gmail.com>
2024-11-15 22:17:24 -0600

    Created main function and set screen display

--- src/project.py
+++ src/project.py
@@ -1,4 +1,13 @@
 import pygame
 
 
+def main():
+    pygame.init()
+    pygame.display.set_caption("Collect the Fruit!")
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+
+
+if __name__ == "__main__":
+    main()
 
